2be7c709ffde8bff8a02057b7e2b9bd663dea344
yaissaoui <aissyass91@gmail.com>
2021-02-10 21:08:50 +0100

    Adding home file

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created: 2021
+ *
+ * @category  SQLI_CustomerAvatar
+ * @package   SQLI
+ * @author    ISC - SQLI MAROC
+ * @copyright 2021 - ISC - SQLI MAROC MAGENTO
+ * @link      https://www.sqli.com/
+ */
+
+function getString()
+{
+    return 'test';
+}
